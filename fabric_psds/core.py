dcd6a6cd50e7b15f76584604575bce3489041691
Chris McDonald <XWraithanX@gmail.com>
2011-09-18 23:01:35 -0700

    list of tasks I need to implement

---
+++ fabric_psds/core.py
@@ -0,0 +1,20 @@
+
+
+@task
+def provision():
+    setup_git()
+    clone_code()
+    setup_nginx()
+    setup_gunicorn()
+    setup_celery()
+    setup_redis()
+    make_virtualenv()
+    sync_db()
+    deploy()
+
+@task
+def deploy():
+    checkout_code()
+    install_requirements()
+    load_code()
+    migrate()
