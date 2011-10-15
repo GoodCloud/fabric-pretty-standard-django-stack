a3073d45d6fe25f199e105e1a9be0e259063faa2
Chris McDonald <XWraithanX@gmail.com>
2011-10-15 02:16:19 -0700

    start of OS specific stuff

--- fabric_psds/__init__.py
+++ fabric_psds/__init__.py
@@ -1,1 +1,21 @@
-print('moo')
+from fabric.api import env
+
+
+env.psds.system = {
+    'ubuntu': {
+        'package install': 'apt-get -y install %(package)s',
+        'service manager': 'invoke-rc.d %(service)s %(action)s',
+        'mkvirtualenv options': '',
+        }
+    'arch linux' {
+        'package install': 'pacman -Sy %(package)s',
+        'service manager': 'rc.d %(action)s %(service)s',
+        'mkvirtualenv options': '-p /usr/bin/python2',
+        }
+    }
+
+env.psds.overrides = {
+    'package install': None
+    'service manager': None
+    'mkvirtualenv options': None
+}
