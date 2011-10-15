9ac4bb2ce403bd249cee68da08f5b51370302990
Chris McDonald <XWraithanX@gmail.com>
2011-10-15 02:35:42 -0700

    cleaned up systems stuff a little

--- fabric_psds/__init__.py
+++ fabric_psds/__init__.py
@@ -1,21 +1,22 @@
 from fabric.api import env
 
 
-env.psds.system = {
-    'ubuntu': {
-        'package install': 'apt-get -y install %(package)s',
-        'service manager': 'invoke-rc.d %(service)s %(action)s',
-        'mkvirtualenv options': '',
-        }
-    'arch linux' {
-        'package install': 'pacman -Sy %(package)s',
-        'service manager': 'rc.d %(action)s %(service)s',
-        'mkvirtualenv options': '-p /usr/bin/python2',
+env.psds = {
+    'systems': {
+        'ubuntu': {
+            'package install': 'apt-get -y install %(package)s',
+            'service manager': 'invoke-rc.d %(service)s %(action)s',
+            'mkvirtualenv options': [],
+            },
+        'arch linux': {
+            'package install': 'pacman -Sy %(package)s',
+            'service manager': 'rc.d %(action)s %(service)s',
+            'mkvirtualenv options': ['-p /usr/bin/python2'],
+            }
+        },
+    'overrides': {
+        'package install': None,
+        'service manager': None,
+        'mkvirtualenv options': None,
         }
     }
-
-env.psds.overrides = {
-    'package install': None
-    'service manager': None
-    'mkvirtualenv options': None
-}
